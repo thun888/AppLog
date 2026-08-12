4168ae72f3aa9e08c24210adebfa4e076d1c94c4
thun888 <i@mail.hzchu.top>
2026-08-12 19:37:46 +0800

    build: 添加阿里云 Maven 仓库

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,15 @@
 pluginManagement {
     repositories {
+        maven {
+            url = uri("https://maven.aliyun.com/repository/google")
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -17,6 +27,8 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
     }
